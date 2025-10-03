7d11ccdc8056f2af0673c2ea5f67b35dcfd8d871
jnicklr <jnichollaslr@gmail.com>
2025-10-03 00:41:16 -0300

    feat: :sparkles: adicionando o módulo do redis e cache na aplicação

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,6 +8,8 @@ import { BankAccountModule } from './modules/bank-account/bank-account.module';
 import { PrometheusModule } from './modules/prometheus/prometheus.module';
 import { CustomLoggerModule } from './shared/custom-logger/custom-logger.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -18,6 +20,16 @@ import { AuthModule } from './modules/auth/auth.module';
     // PrometheusModule,
     CustomLoggerModule,
     AuthModule,
+    CacheModule.registerAsync({
+      isGlobal: true, // deixa disponível em toda a aplicação
+      useFactory: async () => ({
+        store: await redisStore({
+          host: 'localhost',
+          port: 6379,
+          ttl: 60 * 5, // tempo de vida do cache: 5 minutos
+        }),
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
